a3c920e6571629562c7380950b499e538a2f263c
alifzidanr <alifzidanr@gmail.com>
2024-10-17 12:24:36 +0700

    24th commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -58,114 +58,113 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Stack(
-      children: [
-        // Background Image
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/BG.jpg'),
-              fit: BoxFit.cover,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background Image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/BG.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        // Login Form with Logo
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo
-                Image.asset(
-                  'assets/Logo_YPIA.png',
-                  height: 200.0, // Set the height to double the previous size
-                ),
-                SizedBox(height: 30), // Space between logo and form
-                // ID Pegawai Field
-                Container(
-                  height: 50.0,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 6.0,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
+          // Login Form with Logo
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo
+                  Image.asset(
+                    'assets/Logo_YPIA.png',
+                    height: 200.0, // Set the height to double the previous size
                   ),
-                  child: TextField(
-                    controller: _idPegawaiController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.person, color: Colors.grey),
-                      labelText: 'ID Pegawai',
-                      labelStyle: TextStyle(color: Colors.grey),
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  SizedBox(height: 30), // Space between logo and form
+                  // ID Pegawai Field
+                  Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 6.0,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _idPegawaiController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person, color: Colors.grey),
+                        hintText: 'ID Pegawai',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 16),
-                // Password Field
-                Container(
-                  height: 50.0,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 6.0,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock, color: Colors.grey),
-                      labelText: 'Password',
-                      labelStyle: TextStyle(color: Colors.grey),
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  SizedBox(height: 16),
+                  // Password Field
+                  Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 6.0,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
                     ),
-                    obscureText: true,
-                  ),
-                ),
-                SizedBox(height: 20),
-                // Login Button
-                Container(
-                  width: double.infinity, // Match the width of the text fields
-                  height: 50.0, // Similar height to text fields for consistency
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+                    child: TextField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
                       ),
-                      textStyle: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
+                      obscureText: true,
                     ),
-                    onPressed: _login,
-                    child: Text('Login'),
                   ),
-                ),
-              ],
+                  SizedBox(height: 20),
+                  // Login Button
+                  Container(
+                    width: double.infinity, // Match the width of the text fields
+                    height: 50.0, // Similar height to text fields for consistency
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white, // Button color
+                        foregroundColor: Color(0xFF0053C5), // Text color
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        textStyle: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      onPressed: _login,
+                      child: Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
-}
-
-
-
+        ],
+      ),
+    );
+  }
 }
